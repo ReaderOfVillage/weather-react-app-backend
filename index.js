d76ca01399a5aa4072b2020f92e637a41bfb1400
unknown <milk40001@gmail.com>
2022-12-02 10:36:49 -0500

    changed

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const cors = require('cors')
 const express = require('express')
 const weather = require('weather-js')
 const app = express()
-app.use(cors())
 
 app.get('/getweather', async (req, res) => {
     await weather.find({ search: `${req.query.city}, ${req.query.country}`, degreeType: 'F'}, function (err, result) {
@@ -11,6 +9,6 @@ app.get('/getweather', async (req, res) => {
     })
 })
 
-app.listen(4000, () => {
-    console.log(`listening on port ${4000}`)
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })

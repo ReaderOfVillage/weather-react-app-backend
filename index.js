994667d04e52f295aeb612e94c0759ae0d9a7996
unknown <milk40001@gmail.com>
2022-12-02 12:08:16 -0500

    change6

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/getweather', async (req, res) => {
-    console.log(req.query.city)
     await weather.find({ search: `${req.query.city}, ${req.query.country}`, degreeType: 'F'}, function (err, result) {
         if (err) console.log(err)
         res.send(JSON.stringify((result[2])))
